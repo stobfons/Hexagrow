f7ea4a0a3b62dd60fbfa522b3f3ddc4b3f564e4c
stobfons <obberiyageashlysurendrajayashan.fonseka@student.hs-anhalt.de>
2023-06-15 18:23:56 +0200

    Menu-Icon Drag
    Can drag to middle. stay there if perfect, if not go back to the starting position.

--- Spieleprog-01/Assets/Skripts/MenuDrag.cs
+++ Spieleprog-01/Assets/Skripts/MenuDrag.cs
@@ -4,26 +4,39 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.Tilemaps;
 
-public class DragandDrop : MonoBehaviour, IPointerDownHandler,IBeginDragHandler, IDragHandler, IEndDragHandler
+public class MenuDrag : MonoBehaviour, IPointerDownHandler, IBeginDragHandler, IDragHandler, IEndDragHandler
 {
-    [SerializeField] private Canvas canvas;
     [SerializeField] private Tilemap map;
 
-    Transform parentAfterDrag; // set layer of object on the last 
-
     private Vector3 clickedTile;
+    Transform parentAfterDrag;
+
     private RectTransform rectTransform;
+    private Vector3Int dragpos;
+    private Vector3Int goal;
+    private Vector3 startPos;
+
+
     private void Awake()
     {
         rectTransform = GetComponent<RectTransform>();
+
     }
-
-
-
     public void OnBeginDrag(PointerEventData eventData)
     {
         Debug.Log("OnBeginDrag");
         parentAfterDrag = transform.parent;
+
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3Int gridPosition = map.WorldToCell(mousePosition);
+        clickedTile = map.CellToWorld(gridPosition);
+
+        startPos = clickedTile;
+
+        goal.x = Mathf.RoundToInt(4);
+        goal.y = Mathf.RoundToInt(2);
+        goal.z = Mathf.RoundToInt(0);
+
         transform.SetParent(transform.root);
         transform.SetAsLastSibling();
     }
@@ -33,23 +46,27 @@ public class DragandDrop : MonoBehaviour, IPointerDownHandler,IBeginDragHandler,
         Debug.Log("OnDrag");
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector3Int gridPosition = map.WorldToCell(mousePosition);
+        dragpos = gridPosition;
 
         clickedTile = map.CellToWorld(gridPosition);
         print("AT 1st position " + gridPosition[0] + "AT 2nd position " + gridPosition[1] + "AT 3rd position " + gridPosition[2]);
         rectTransform.position = Input.mousePosition;
     }
-
     public void OnEndDrag(PointerEventData eventData)
     {
         Debug.Log("OnEndDrag");
-        rectTransform.position = clickedTile;
-        transform.SetParent(parentAfterDrag);
+        if (dragpos == goal)
+        {
+            rectTransform.position = clickedTile;
+            transform.SetParent(parentAfterDrag);
+        }
+        else
+        {
+            rectTransform.position = startPos;
+        }
     }
-
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("OnPointerDown");
     }
-
-
 }
