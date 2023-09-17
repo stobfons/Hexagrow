82b9051a94c1235324935322cf8b5353ad1d26e3
stobfons <obberiyageashlysurendrajayashan.fonseka@student.hs-anhalt.de>
2023-09-17 14:50:51 +0200

    resetButton+ co.

---
+++ Hexagrow/Assets/Skripts/ResetGame.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class ResetGame : MonoBehaviour
+
+       
+{
+    public Button yourButton;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+    static void TaskOnClick()
+    {
+        Loader.newGame();
+        SceneManager.LoadScene("Menu");
+    }
+}
